cde9c1068f8d884cb1be16c2a922094dae126d14
itspradeep.singh@outlook.com <itspradeep.singh@outlook.com>
2023-02-27 15:17:53 +0000

    Initial Notebook

---
+++ 01.py
@@ -0,0 +1,8 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC This is the first notebook that uses DBR 7.3 .
+# MAGIC  - This DBR would allow us to test the most unoptimized code in DBX
+
+# COMMAND ----------
+
+
